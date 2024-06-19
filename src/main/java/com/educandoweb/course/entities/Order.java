119cbb91eaa7cbe0c6ad4e0016451e38a175f99a
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-19 10:48:18 -0300

    feat: Order, instant, ISO 8601

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,44 @@
+package com.educandoweb.course.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    private static final Long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId")
+    private User client;
+
+    public Order() {
+
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client= client;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
