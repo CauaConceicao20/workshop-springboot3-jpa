8a9bb7a820351b11806f807640a70747429f770a
CauaConceicao20 <CauaConceicaoDev@hotmail.com>
2024-06-20 17:22:51 -0300

    feat: User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -4,10 +4,13 @@ import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.service.OrderService;
 import com.educandoweb.course.service.UserService;
+import jakarta.transaction.Transactional;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +34,17 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/insert")
+    @Transactional
+    public ResponseEntity<User> insertUser(@RequestBody User obj, UriComponentsBuilder uriBuilder) {
+        User user = service.insert(obj);
+
+        var uri = uriBuilder.path("/user/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(user);
+
     }
 
 }
